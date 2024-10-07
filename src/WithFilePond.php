3a60d0346641978ddde1980cdeab09a1f3401af5
Rias <hey@rias.be>
2024-10-07 09:24:02 +0200

    Set property to empty array when removing file

--- src/WithFilePond.php
+++ src/WithFilePond.php
@@ -14,6 +14,7 @@ trait WithFilePond
     public function remove($property, $filename): void
     {
         $file = Str::after($filename, config('app.url'));
+        $this->$property = [];
 
         File::delete(public_path($file));
     }
